fd2be4599df8cb9913ef12957adc92099de72556
Phayce04 <dhenielpontiga@gmail.com>
2025-01-08 20:59:18 +0800

    Served the index html and change the type to module

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
-const express=require('express')
+import express from 'express'
+import path, {dirname} from 'path'
+import { fileURLToPath } from 'url';
 const app =express();
 const PORT=process.env.PORT||3004
+//json requests
+app.use(express.json())
+// html files
+app.use(express.static('./public'))
 
 
-app.get('/',()=>{
-  
+const  __filename=fileURLToPath(import.meta.url)
+const __dirname=dirname(__filename)
+
+// home page
+app.get('/',(req,res)=>{
+res.sendFile(path.join(__dirname,'public','index.html'))
 })
+
+//api routes  
 app.get('/api/words',()=>{
   res.send("words")
 })
